71e5984c7a50c64ba6c5fcfa2ba52bde67795969
Kyriakos Nicola <knicola@users.noreply.github.com>
2021-08-12 21:31:54 -0400

    add start, end and helper methods to assessment service

--- services/api-server/src/services/assessment.js
+++ services/api-server/src/services/assessment.js
@@ -2,6 +2,7 @@
 
 const Assessment = require('../models/assessment')
 const uuid = require('uuid')
+const { DateTime } = require('luxon')
 
 /**
  * Create new Assessment record
@@ -11,11 +12,11 @@ const uuid = require('uuid')
  * @returns {Promise<any>} Assessment record
  */
 async function create(examId, takerId) {
-    return Assessment.query().insert({
+    return Assessment.query().insertAndFetch({
         exam_id: examId,
         taker_id: takerId,
         session: uuid.v4(),
-    }).returning('*')
+    })
 }
 
 /**
@@ -28,7 +29,36 @@ function findBySession(session) {
     return Assessment.query().where('session', session).first()
 }
 
+async function startBySession(session) {
+    return Assessment.query()
+        .where('session', session)
+        .whereNull('started_at')
+        .whereNull('submitted_at')
+        .patch({ started_at: Assessment.fn.now() })
+        .returning('*')
+        .first()
+}
+
+async function endBySession(session) {
+    return Assessment.query()
+        .where('session', session)
+        .whereNotNull('started_at')
+        .whereNull('submitted_at')
+        .patch({ submitted_at: Assessment.fn.now() })
+        .returning('*')
+        .first()
+}
+
+const isExpired = (assessment) => DateTime.fromSQL(assessment.started_at).plus({ hour: 1 }) <= DateTime.now()
+const isCompleted = (assessment) => !! assessment.submitted_at
+const isAccessible = (assessment) => ! isCompleted(assessment) && ! isExpired(assessment)
+
 module.exports = {
     create,
     findBySession,
+    startBySession,
+    isExpired,
+    isCompleted,
+    isAccessible,
+    endBySession,
 }
--- services/api-server/tests/services/assessment.spec.js
+++ services/api-server/tests/services/assessment.spec.js
@@ -2,10 +2,12 @@
 
 const faker = require('faker')
 const Assessment = require('../../src/models/assessment')
+const db = require('../../src/database')
 const assessmentService = require('../../src/services/assessment')
+const { DateTime } = require('luxon')
 
 describe('[intgr] services/taker.js', () => {
-    afterAll(async () => await Assessment.knex().destroy())
+    afterAll(async () => await db.destroy())
     describe('.create()', () => {
         it('should insert a new record and return the result', async () => {
             const record = {
@@ -36,5 +38,80 @@ describe('[intgr] services/taker.js', () => {
 
             expect(res).toEqual(insertRes)
         }) // test
+    }) // group
+    describe('.startBySession()', () => {
+        it('should start assessment', async () => {
+            const assessmentRes = await Assessment.query().insertAndFetch({
+                taker_id: 1,
+                exam_id: 1,
+                session: faker.datatype.uuid(),
+            })
+            jest.spyOn(Assessment.fn, 'now').mockImplementationOnce(() => '2021-08-12T17:41:36.019Z')
+            const res = await assessmentService.startBySession(assessmentRes.session)
+
+            expect(res).toMatchObject({
+                started_at: new Date('2021-08-12T17:41:36.019Z'),
+                submitted_at: null,
+            })
+        }) //test
+    }) // group
+    describe('.endBySession()', () => {
+        it('should end assessment', async () => {
+            const assessmentRes = await Assessment.query().insertAndFetch({
+                taker_id: 1,
+                exam_id: 1,
+                session: faker.datatype.uuid(),
+                started_at: '2021-08-12T17:41:36.019Z',
+            })
+            jest.spyOn(Assessment.fn, 'now').mockImplementationOnce(() => '2021-08-12T18:01:47.269Z')
+            const res = await assessmentService.endBySession(assessmentRes.session)
+
+            expect(res).toMatchObject({
+                started_at: new Date('2021-08-12T17:41:36.019Z'),
+                submitted_at: new Date('2021-08-12T18:01:47.269Z'),
+            })
+        }) //test
+    }) // group
+    describe('.isExpired()', () => {
+        it('should return false if time is up', () => {
+            const assessment = { started_at: DateTime.now().toSQL() }
+            const res = assessmentService.isExpired(assessment)
+            expect(res).toBe(false)
+        })
+        it('should return true if time is not up', () => {
+            const assessment = { started_at: DateTime.now().minus({ hour: 1 }).toSQL() }
+            const res = assessmentService.isExpired(assessment)
+            expect(res).toBe(true)
+        })
+    }) // group
+    describe('.isCompleted()', () => {
+        it('should return false if assessment has not been submitted', () => {
+            const assessment = { submitted_at: null }
+            const res = assessmentService.isCompleted(assessment)
+            expect(res).toBe(false)
+        })
+        it('should return true if assessment has been submitted', () => {
+            const assessment = { submitted_at: DateTime.now().toSQL() }
+            const res = assessmentService.isCompleted(assessment)
+            expect(res).toBe(true)
+        })
+    }) // group
+    describe('.isAccessible()', () => {
+        it('should return false if time is up or assessment has been submitted', () => {
+            // time's up
+            const assessment1 = { started_at: DateTime.now().minus({ hour: 1 }).toSQL(), submitted_at: null }
+            const res1 = assessmentService.isAccessible(assessment1)
+            expect(res1).toBe(false)
+
+            // submitted
+            const assessment2 = { started_at: DateTime.now().toSQL(), submitted_at: DateTime.now().toSQL() }
+            const res2 = assessmentService.isAccessible(assessment2)
+            expect(res2).toBe(false)
+        })
+        it('should return true if assessment has not expired and has not been submitted', () => {
+            const assessment = { started_at: DateTime.now().toSQL(), submitted_at: null }
+            const res = assessmentService.isAccessible(assessment)
+            expect(res).toBe(true)
+        })
     }) // group
 }) // group
